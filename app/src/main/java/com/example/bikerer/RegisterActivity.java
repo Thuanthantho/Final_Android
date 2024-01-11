6935c8967d1b6379993e92af370735422a69a828
Thuanthantho <86817559+Thuanthantho@users.noreply.github.com>
2024-01-11 17:19:52 +0700

    Update RegisterActivity.java

--- app/src/main/java/com/example/bikerer/RegisterActivity.java
+++ app/src/main/java/com/example/bikerer/RegisterActivity.java
@@ -3,44 +3,44 @@ package com.example.bikerer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.MenuItem;
-import androidx.appcompat.widget.Toolbar;
-
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     TextView alreadyHaveaccount;
-    EditText inputEmail,inputPassword,inputConfirmPassword;
+    EditText inputEmail, inputPassword, inputConfirmPassword;
     Button btnRegister;
-    String emailPattern= "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     ProgressDialog progressDialog;
 
     FirebaseAuth mAuth;
     FirebaseUser mUser;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
 
         alreadyHaveaccount = findViewById(R.id.alreadyHaveaccount);
 
@@ -54,13 +54,14 @@ public class RegisterActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        inputEmail=findViewById(R.id.inputEmail);
-        inputPassword=findViewById(R.id.inputPassword);
-        inputConfirmPassword=findViewById(R.id.inputConfirmPassword);
-        btnRegister=findViewById(R.id.btnRegister);
-        progressDialog= new ProgressDialog(this);
-        mAuth= FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
 
         alreadyHaveaccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,42 +73,52 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PerforAuth();
+                performAuth();
             }
         });
     }
 
-    private void PerforAuth() {
-        String email=inputEmail.getText().toString();
-        String password=inputPassword.getText().toString();
-        String confirmPassword=inputConfirmPassword.getText().toString();
+    private void performAuth() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
 
-        if(!email.matches(emailPattern)){
+        if (!email.matches(emailPattern)) {
             inputEmail.setError("Enter Correct Your Email");
-        }else if (password.isEmpty() || password.length()<6){
+        } else if (password.isEmpty() || password.length() < 6) {
             inputPassword.setError("Enter Correct Your Password");
-        }else if (!password.equals(confirmPassword)){
+        } else if (!password.equals(confirmPassword)) {
             inputConfirmPassword.setError("Passwords not match");
-        }else{
-            progressDialog.setMessage("Pleas Wait");
+        } else {
+            progressDialog.setMessage("Please Wait");
             progressDialog.setTitle("Registering");
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
 
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         progressDialog.dismiss();
-                        backtoMainActivity();
-                        Toast.makeText(RegisterActivity.this, "Register Succesfully", Toast.LENGTH_SHORT).show();
-                    }else{
+
+                        // Save user data to Realtime Database
+                        saveUserDataToDatabase(email);
+
+                        backToMainActivity();
+                        Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
                         progressDialog.dismiss();
-                        Toast.makeText(RegisterActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegisterActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
+    }
+
+    private void saveUserDataToDatabase(String email) {
+        String userId = mAuth.getCurrentUser().getUid();
+        User user = new User(email);
+        mDatabase.child(userId).setValue(user);
     }
 
     @Override
@@ -119,10 +130,9 @@ public class RegisterActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void backtoMainActivity(){
-        Intent intent  =new Intent(RegisterActivity.this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+    private void backToMainActivity() {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
-
 }
