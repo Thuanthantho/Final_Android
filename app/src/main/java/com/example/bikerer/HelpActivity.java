8b31e64d11bebb23cd6c29470fd38f14fea560af
Viznem <51557569+Viznem@users.noreply.github.com>
2024-01-17 14:32:20 +0700

    add help

--- app/src/main/java/com/example/bikerer/HelpActivity.java
+++ app/src/main/java/com/example/bikerer/HelpActivity.java
@@ -7,14 +7,18 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class HelpActivity extends AppCompatActivity {
-    private LinearLayout btn_help1, layout_help1;
+    private LinearLayout btn_help1, btn_help2,btn_help3, layout_help1, layout_help2,layout_help3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
         btn_help1 = findViewById(R.id.btn_help1);
+        btn_help2 = findViewById(R.id.btn_help2);
+        btn_help3 = findViewById(R.id.btn_help3);
         layout_help1 = findViewById(R.id.layout_toggle_help1);
+        layout_help2 = findViewById(R.id.layout_toggle_help2);
+        layout_help3 = findViewById(R.id.layout_toggle_help3);
 
         btn_help1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +28,30 @@ public class HelpActivity extends AppCompatActivity {
                     layout_help1.setVisibility(View.GONE);
                 }else {
                     layout_help1.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btn_help2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int isVisible = layout_help2.getVisibility();
+                if(isVisible==View.VISIBLE){
+                    layout_help2.setVisibility(View.GONE);
+                }else {
+                    layout_help2.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btn_help3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int isVisible = layout_help3.getVisibility();
+                if(isVisible==View.VISIBLE){
+                    layout_help3.setVisibility(View.GONE);
+                }else {
+                    layout_help3.setVisibility(View.VISIBLE);
                 }
             }
         });
