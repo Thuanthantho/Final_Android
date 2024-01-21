ad7acc19c3d77bd6104edb0de3d5ee283f5bd947
Viznem <51557569+Viznem@users.noreply.github.com>
2024-01-21 17:51:28 +0700

    Fix Driver bugs

---
+++ app/src/main/java/com/example/bikerer/OnTheWayActivity.java
@@ -0,0 +1,14 @@
+package com.example.bikerer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnTheWayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_the_way);
+    }
+}
